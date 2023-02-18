b232d5a6abd45bbf0baa401beb452d3a3423802f
Amsterdance26 <amsterdance26@gmail.com>
2023-02-19 01:45:55 +0700

    End Project

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -15,56 +15,71 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak var splitNumberLabel: UILabel!
     @IBOutlet weak var twentyPctButton: UIButton!
     @IBOutlet weak var stepper: UIStepper!
-    
-    @IBAction func tipChanged(_ sender: UIButton) {
-        billTextField.endEditing(true)
-        zeroPctButton.isSelected = false
-        tenPctButton.isSelected = false
-        twentyPctButton.isSelected = false
-        
-        sender.isSelected = true
-        
-        if sender == zeroPctButton {
-            print("Zero percent button pressed: 0.0")
-        } else if sender == tenPctButton {
-            print("Ten percent button pressed: 0.1")
-        } else if sender == twentyPctButton {
-            print("Twenty percent button pressed: 0.2")
+
+    var amountPerPerson: String?
+        var totalAmount: String?
+
+        @IBAction func tipChanged(_ sender: UIButton) {
+            billTextField.endEditing(true)
+            zeroPctButton.isSelected = false
+            tenPctButton.isSelected = false
+            twentyPctButton.isSelected = false
+
+            sender.isSelected = true
+
+            if sender == zeroPctButton {
+                print("Zero percent button pressed: 0.0")
+            } else if sender == tenPctButton {
+                print("Ten percent button pressed: 0.1")
+            } else if sender == twentyPctButton {
+                print("Twenty percent button pressed: 0.2")
+            }
+            sender.isSelected = true
+
+
         }
-        sender.isSelected = true
-        
-        
-    }
-    
-    @IBAction func stepperValueChanged(_ sender: UIStepper) {
-        splitNumberLabel.text = Int(sender.value).description
-    }
-    
-    @IBAction func calculatePressed(_ sender: UIButton) {
-        let splitNumber = Int(splitNumberLabel.text ?? "1") ?? 1
-        guard let billAmount = Double(billTextField.text ?? "") else {
-            print("Invalid bill amount")
-            return
+
+        @IBAction func stepperValueChanged(_ sender: UIStepper) {
+            splitNumberLabel.text = Int(sender.value).description
         }
-        
-        print("Bill Amount: \(billAmount)")
-        print("Split Number: \(splitNumber)")
-        
-        let tipPercentage: Double
-        
-        if tenPctButton.isSelected {
-            tipPercentage = 0.1
-        } else if twentyPctButton.isSelected {
-            tipPercentage = 0.2
-        } else {
-            tipPercentage = 0.0
+
+        @IBAction func calculatePressed(_ sender: UIButton) {
+            let splitNumber = Int(splitNumberLabel.text ?? "1") ?? 1
+            guard let billAmount = Double(billTextField.text ?? "") else {
+                print("Invalid bill amount")
+                return
+            }
+
+            print("Bill Amount: \(billAmount)")
+            print("Split Number: \(splitNumber)")
+
+            let tipPercentage: Double
+
+            if tenPctButton.isSelected {
+                tipPercentage = 0.1
+            } else if twentyPctButton.isSelected {
+                tipPercentage = 0.2
+            } else {
+                tipPercentage = 0.0
+            }
+
+            let totalAmount = billAmount * (1 + tipPercentage)
+            let amountPerPerson = totalAmount / Double(splitNumber)
+            let formattedAmount = String(format: "%.2f", amountPerPerson)
+
+            self.amountPerPerson = formattedAmount
+            self.totalAmount = String(format: "%.2f", totalAmount)
+
+            self.performSegue(withIdentifier: "goToResults", sender: self)
         }
-        
-        let totalAmount = billAmount * (1 + tipPercentage)
-        let amountPerPerson = totalAmount / Double(splitNumber)
-        let formattedAmount = String(format: "%.2f", amountPerPerson)
-        
-        print("Total Amount: \(totalAmount)")
-        print("Amount per person: \(formattedAmount)")
+
+        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            if segue.identifier == "goToResults" {
+                let destinationVC = segue.destination as! ResultsViewController
+                destinationVC.amountPerPerson = self.amountPerPerson
+                destinationVC.totalAmount = self.totalAmount
+            }
+        }
     }
-}
+
+   
--- Tipsy/Controllers/ResultsViewController.swift
+++ Tipsy/Controllers/ResultsViewController.swift
@@ -8,17 +8,27 @@
 import UIKit
 
 class ResultsViewController: UIViewController {
-    
+
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var settingsLabel: UILabel!
-    
+
+    var amountPerPerson: String?
+    var totalAmount: String?
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+
+        if let amountPerPerson = self.amountPerPerson, let totalAmount = self.totalAmount {
+            totalLabel.text = amountPerPerson
+            settingsLabel.text = "Total amount: \(totalAmount)"
+        }
     }
-    @IBAction func recalculatePressed( sender: UIButton) {
-        
+
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
     }
-    
 }
+
+
+
+
